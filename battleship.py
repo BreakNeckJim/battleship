ef5fe0f4264133e884e9f910bf5a2e0a79cd31bb
Jesse Wagner <jesse.wagner@outlook.com>
2022-07-09 01:22:06 -0300

    Added board gen and vertical piece gen

---
+++ battleship.py
@@ -0,0 +1,27 @@
+import random
+import math
+
+class Board:
+    def __init__(self):
+        self.spaces = [*range(1,101)]
+        self.board_spaces = {}
+        self.stored_vert_nums = []
+    def __repr__(self):
+        return "This is the game board, it contains modules for producing vertical and horizontal pieces, as well as the game board itself!"
+    def get_vertical(self):
+        num = random.randint(1, 100)
+        print(num)
+        stored_vert_nums = []
+        stored_vert_nums.append(num)
+        if (num+10) < 100:
+            stored_vert_nums.append(num+10)
+        else:
+            stored_vert_nums.append(num-10)
+        if (num+20) < 100:
+            stored_vert_nums.append(num+20)
+        else:
+            stored_vert_nums.append(num-20)
+        return stored_vert_nums
+board_1 = Board()
+board_1.get_vertical()
+print(board_1.stored_vert_nums)
